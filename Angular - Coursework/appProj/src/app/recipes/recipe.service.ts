af948b301ae029cf2f84a1d23f059a0960726fd4
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-09-12 12:26:22 +0530

    sep12-1

--- Angular - Coursework/appProj/src/app/recipes/recipe.service.ts
+++ Angular - Coursework/appProj/src/app/recipes/recipe.service.ts
@@ -1,15 +1,36 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple a test', 'https://i.pinimg.com/736x/ff/c3/d3/ffc3d3f7e25c28ea2d3fe42231736f00--vector-clipart-bacon.jpg'),
-    new Recipe('Another Test Recipe', 'This is simple a test', 'http://www.clipartbest.com/cliparts/ecM/ygp/ecMygp6bi.jpg')
+    new Recipe('Eggs n Bacon',
+    'To enlighten your morning!  ',
+    'https://i.pinimg.com/736x/ff/c3/d3/ffc3d3f7e25c28ea2d3fe42231736f00--vector-clipart-bacon.jpg',
+    [
+      new Ingredient('Sausage', 3),
+      new Ingredient('Fries', 1)
+    ]),
+    new Recipe('Sandwich',
+    'Jammed with layers!',
+    'http://www.clipartbest.com/cliparts/ecM/ygp/ecMygp6bi.jpg',
+    [
+      new Ingredient('Cheese', 2),
+      new Ingredient('Patty', 1)
+    ])
   ];
+
+  constructor(private slService: ShoppingListService){}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- Angular - Coursework/appProj/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular - Coursework/appProj/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
@@ -8,9 +9,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amoutInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +19,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amoutInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
 
 }
