2c852fa2f819dd0e46a7d1e952e1b6f70a240fc5
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-09-14 09:06:08 +0530

    sep14

--- routes/myapp/src/app/app.module.ts
+++ routes/myapp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule, Router } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -12,14 +11,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UserComponent},
-  { path: 'users/:id/:name', component: UserComponent},
-  { path: 'servers', component: ServersComponent},
-  { path: 'servers/:id/edit', component: EditServerComponent},
-];
 
 @NgModule({
   declarations: [
@@ -29,12 +23,13 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
