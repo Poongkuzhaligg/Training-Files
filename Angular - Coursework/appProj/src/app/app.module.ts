293b7832a44fd46517327c808f9be722d1a28e2d
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-09-15 14:44:42 +0530

    sep15

--- Angular - Coursework/appProj/src/app/app.module.ts
+++ Angular - Coursework/appProj/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -23,13 +25,15 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     RecipetItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routes/myapp/src/app/app-routing.module.ts
+++ routes/myapp/src/app/app-routing.module.ts
@@ -7,15 +7,23 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
+import { AuthGuard } from "./auth.guard.service";
+import { canDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UserComponent, children: [
     { path: ':id/:name', component: UserComponent}
   ] },
-  { path: 'servers', component: ServersComponent, children: [
+  {
+    path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
     { path: ':id', component: ServerComponent},
-    { path: ':id/edit', component: EditServerComponent}
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [canDeactivateGuard]}
   ] },
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' }
