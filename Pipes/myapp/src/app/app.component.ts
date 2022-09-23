45c134101d2662cb5834144b4025b867c79291ca
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-09-23 10:07:12 +0530

    sep23-0

--- Pipes/myapp/src/app/app.component.ts
+++ Pipes/myapp/src/app/app.component.ts
@@ -6,11 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
   servers = [
     {
       instanceType: 'medium',
-      name: 'Production',
+      name: 'Production Server',
       status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Quality Assurance Server',
+      status: 'critical',
       started: new Date(15, 1, 2017)
     },
     {
@@ -39,5 +50,14 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    })
   }
 }
