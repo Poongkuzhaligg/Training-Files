589ca5c89f49f21293500b8a155ca203aea378f0
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-10-27 13:46:10 +0530

    oct27

--- ggProducts/src/app/account/account.service.ts
+++ ggProducts/src/app/account/account.service.ts
@@ -1,20 +1,19 @@
-import { HttpClient, JsonpClientBackend } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 import { User } from '../user';
+import { Storage } from '@capacitor/storage';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  users: any[];
-  apiUrl: string;
-  public user: Observable<User>;
+  currentUser: User;
+  users: User[] = [];
 
   constructor(private http: HttpClient) {
+    this.getObject();
    }
 
   login(email: string, password: string){
@@ -22,13 +21,41 @@ export class AccountService {
 
   }
 
-  addUser(userD: User) {
-      return this.http.post(`/account/register`, userD);
-      console.log(userD);
+  register(userD: User) {
+    const foundUser = this.users.find( user => user.email === userD.email);
+    this.currentUser = userD;
+  }
+
+
+  async setObject() {
+    await Storage.set({
+      key: 'user',
+      value: JSON.stringify(this.users)
+    });
+  }
+
+  async getObject() {
+    const ret = await Storage.get({ key: 'users' });
+    const user = JSON.parse(ret.value);
+    console.log('user', ret);
+    if(ret !== null){
+      this.addUser(user);
+    }
+  }
+
+  async addUser(userD: User) {
+    if(this.users.length === 0){
+      this.users.push(userD);
+    }
+    else {
+      this.users = [userD, ...this.users];
+    }
+    await this.setObject();
+    console.log(this.users);
   }
 
   getAll() {
-    return this.http.get<User[]>(`/home`);
+
   }
 
 
